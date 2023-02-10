b9ff3976450954c427bfc2f7acb06d04e5619e3e
Vladislav Samorukov <smrk.vld@gmail.com>
2023-02-10 16:39:55 +0300

    done

--- src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
+++ src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
@@ -2,6 +2,7 @@ package lang.print.gaps.task3;
 
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
-        //write code here
+        int first = 1, second = 10, third = 100;
+        System.out.println(first + "\n" + second + "\n" + third);
     }
 }
--- src/main/java/lang/print/gaps/task3/ReassigningValues.java
+++ src/main/java/lang/print/gaps/task3/ReassigningValues.java
@@ -2,6 +2,12 @@ package lang.print.gaps.task3;
 
 public class ReassigningValues {
     public static void main(String[] args) {
-        //write code here
+        int first = 1, second = 10, third = 100;
+        System.out.println(first + "\n" + second + "\n" + third);
+        int linkToFirst = first, linkToSecond = second, linkToThird = 100;
+        first = 15;
+        second = 6;
+        third = 4;
+        System.out.println(first + "\n" + second + "\n" + third + "\n" + linkToFirst + "\n" + linkToSecond + "\n" + linkToThird);
     }
 }
